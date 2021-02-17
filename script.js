8e99a0d3b61b7bd0faad880bf2f19a57431986e0
Radu Andrei Mihaila <52541540+gloriell@users.noreply.github.com>
2021-02-17 22:27:03 +0200

    js complete

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ const accordeonContent = document.querySelectorAll(
   ".container__wrapper__text__item__accordeon"
 );
 
+function addClass(el, klass) {
+  el.classList.add(klass);
+}
+
+function removeClass(el, klass) {
+  el.classList.remove(klass);
+}
+
 accordeon.forEach((accordeon) => {
   const accordeonTitleRow = accordeon.firstElementChild;
 
   accordeonTitleRow.addEventListener("click", (e) => {
     accordeonContent.forEach((content) => {
       if (content.previousElementSibling === e.target) {
-        content.classList.remove("change");
+        content.classList.remove("hidden");
         content.parentElement.classList.add("active");
+        // removeClass(content, "hidden");
+        // addClass(content.parentElement, "active");
       } else {
-        content.classList.add("active");
-        content.parentElement.classList.remove("change");
+        content.parentElement.classList.remove("active");
+        content.classList.add("hidden");
+        // removeClass(content.parentElement, "active");
+        // addClass(content, "hidden");
+
+        console.log(content.parentElement);
       }
     });
   });
